dfcf7d824f6953ecc68958f5876211c1e760b7d9
heisumar25 <umarabdulgafar76@gmail.com>
2024-09-20 16:54:57 +0100

    Initial commit

---
+++ Dribble/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import Nav from './Nav'
+import Hero from './Hero'
+function App() {
+
+  return (
+    <>
+      <Nav/>
+      <Hero/>
+    </>
+  )
+}
+
+export default App
